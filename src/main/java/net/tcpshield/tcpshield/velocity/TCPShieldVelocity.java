6a518ea80c1753e39a6bb27c144be47944dbc25f
DayHawk <realbattle@t-online.de>
2020-07-07 19:25:44 +0200

    Code CleanUp

--- src/main/java/net/tcpshield/tcpshield/velocity/TCPShieldVelocity.java
+++ src/main/java/net/tcpshield/tcpshield/velocity/TCPShieldVelocity.java
@@ -4,10 +4,8 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
-import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 
-import java.nio.file.Path;
 import java.util.logging.Logger;
 
 @Plugin(id = "tcpshield",
